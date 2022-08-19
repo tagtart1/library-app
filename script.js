dba3442042b0dfd45eccd3899bfe649c48b5ff02
tagtart1 <adamtbizz@gmail.com>
2022-08-18 21:44:08 -0500

    Add ability to display cards into a dynamic grid

--- script.js
+++ script.js
@@ -13,21 +13,45 @@ Book.prototype.toggleRead = function() {
     this.isRead = !this.isRead;
 }
 
-const formSubmitBtn = document.getElementById('form-button');
-const newBookBtn = document.querySelector('.new-book-btn');
+
 
 
 const addBookToLibrary = (e) => {
+    
     e.preventDefault();
     const newBook = getBookFromInput();
 
     myLibrary.push(newBook);
+    loopAndDisplayBooks();
     resetAndCloseForm();
     
-    
-    
-    
 }
+
+function loopAndDisplayBooks() {
+        
+    const libContainer = document.querySelector('.library-container');
+    const cardItem = document.createElement('div');
+    const titlePara = document.createElement('p');
+    const authorPara = document.createElement('p');
+    const pagesPara = document.createElement('p');
+    const submitBtn = document.createElement('button');
+
+
+    cardItem.classList.add('card-item');
+
+    titlePara.textContent = `"${myLibrary[myLibrary.length - 1].title}"`;
+    authorPara.textContent = `${myLibrary[myLibrary.length - 1].author}`;
+    pagesPara.textContent = `${myLibrary[myLibrary.length - 1].pages} pages`;
+    submitBtn.textContent = `Read`;
+
+    cardItem.appendChild(titlePara);
+    cardItem.appendChild(authorPara);
+    cardItem.appendChild(pagesPara);
+    cardItem.appendChild(submitBtn);
+
+    libContainer.appendChild(cardItem);
+}
+
 
 function getBookFromInput() {
     let titleValue = document.querySelector("#title").value;
@@ -39,14 +63,17 @@ function getBookFromInput() {
 
 
 function resetAndCloseForm() {
-    document.querySelector('.form').reset();
+    form.reset();
     const popup = document.querySelector('.popup-container');
     popup.style.display ='none';
 }
 
 
 
+const form = document.querySelector('.form');
+const newBookBtn = document.querySelector('.new-book-btn');
 
+form.onsubmit = addBookToLibrary;
 
 newBookBtn.addEventListener('click', () => {
     const popup = document.querySelector('.popup-container');
@@ -61,7 +88,9 @@ window.onclick = function (e) {
     }
 }
 
-formSubmitBtn.onsubmit = addBookToLibrary;
+
+
+
 
 
 
