447c46dab484cfc3a42619e050ee7b84591ef92c
tagtart1 <adamtbizz@gmail.com>
2022-08-22 18:45:48 -0500

    Add full fuctionality to add and delete books
    Every aspect to adding book objects with styling

--- script.js
+++ script.js
@@ -22,29 +22,43 @@ const addBookToLibrary = (e) => {
     const newBook = getBookFromInput();
 
     myLibrary.push(newBook);
-    loopAndDisplayBooks();
+    addAndDisplayBook(newBook);
     resetAndCloseForm();
     
 }
 
-function loopAndDisplayBooks() {
+function addAndDisplayBook(newBook) {
         
     const libContainer = document.querySelector('.library-container');
     const cardItem = document.createElement('div');
     const titlePara = document.createElement('p');
+    const deleteIcon = document.createElement('img');
     const authorPara = document.createElement('p');
     const pagesPara = document.createElement('p');
     const submitBtn = document.createElement('button');
 
 
     cardItem.classList.add('card-item');
+    deleteIcon.classList.add('delete-icon');
 
-    titlePara.textContent = `"${myLibrary[myLibrary.length - 1].title}"`;
-    authorPara.textContent = `${myLibrary[myLibrary.length - 1].author}`;
-    pagesPara.textContent = `${myLibrary[myLibrary.length - 1].pages} pages`;
-    submitBtn.textContent = `Read`;
+    titlePara.textContent = `"${newBook.title}"`;
+    deleteIcon.src = "delete.svg"; deleteIcon.alt="delete icon";
+    
+    authorPara.textContent = `${newBook.author}`;
+    pagesPara.textContent = `${newBook.pages} pages`;
 
+
+    submitBtn.textContent = newBook.isRead ? 'Read' : "Not read";
+
+    if(newBook.isRead)  submitBtn.classList.add('have-read');
+    
+
+    submitBtn.addEventListener('click', toggleBtnRead )
+    deleteIcon.addEventListener('click', deleteBook )
+
+    cardItem.setAttribute('data-index', `${myLibrary.length - 1}`);
     cardItem.appendChild(titlePara);
+    cardItem.appendChild(deleteIcon);
     cardItem.appendChild(authorPara);
     cardItem.appendChild(pagesPara);
     cardItem.appendChild(submitBtn);
@@ -58,9 +72,30 @@ function getBookFromInput() {
     let authorValue = document.querySelector("#author").value;
     let pagesValue = document.querySelector("#pages").value;
     let isReadValue = document.querySelector("#haveRead").checked;
-    return new Book(titleValue, authorValue, pagesValue, isReadValue);
+    return new Book( authorValue, titleValue, pagesValue, isReadValue);
 }
 
+function toggleBtnRead(e) {
+    myLibrary[e.target.parentElement.getAttribute('data-index')].toggleRead();
+    e.target.textContent = myLibrary[e.target.parentElement.getAttribute('data-index')].isRead ? 'Read' : "Not read";
+    e.target.classList.toggle("have-read");
+}
+
+function deleteBook(e) {
+    //remove from array library
+    myLibrary.splice(e.currentTarget.parentElement.getAttribute('data-index'), 1);
+    //remove from DOM
+    e.currentTarget.parentElement.remove();
+    //adjust all data indexes to accurate resemble its index in library array
+    cardItems = document.querySelectorAll('.card-item');
+    cardItems.forEach((cardItem) => {
+        if(cardItem.dataset.index == 0) return;
+        
+        else {
+            cardItem.dataset.index = cardItem.dataset.index - 1
+        }
+    });
+}
 
 function resetAndCloseForm() {
     form.reset();
@@ -72,6 +107,7 @@ function resetAndCloseForm() {
 
 const form = document.querySelector('.form');
 const newBookBtn = document.querySelector('.new-book-btn');
+
 
 form.onsubmit = addBookToLibrary;
 
