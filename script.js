b79dea7c39a4e10eb2d9b4657a1607a04683d347
tagtart1 <adamtbizz@gmail.com>
2022-08-25 11:13:19 -0500

    Cleared global scope and moved method to class

--- script.js
+++ script.js
@@ -1,36 +1,88 @@
 
 
-let myLibrary = [];
 
-function Book(author, title, pages, isRead) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.isRead = isRead;
+
+class Book {
+
+    constructor(author, title, pages, isRead) {
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.isRead = isRead;
+    }
+
+    toggleRead = () => {
+        this.isRead = !this.isRead;
+    }
+   
 }
 
-Book.prototype.toggleRead = function() {
-    this.isRead = !this.isRead;
-}
+class Library {
+    myLibrary = [];
 
+    form = document.querySelector('.form');
+    newBookBtn = document.querySelector('.new-book-btn');
 
+    constructor() {
+        
 
+        this.newBookBtn.addEventListener('click', () => {
+            const popup = document.querySelector('.popup-container');
+            popup.style.display ='block';
+            
+        });
+    }
 
-const addBookToLibrary = (e) => {
-    //stops webpage from refreshing when submitting
-    e.preventDefault();
-    const newBook = getBookFromInput();
+    get form() {
+        return this.form;
+    }
 
-    myLibrary.push(newBook);
-    //create DOM book card
-    addAndDisplayBook(newBook);
-    //empties form upon submission
-    resetAndCloseForm();
+    getBookFromInput() {
+        let titleValue = document.querySelector("#title").value;
+        let authorValue = document.querySelector("#author").value;
+        let pagesValue = document.querySelector("#pages").value;
+        let isReadValue = document.querySelector("#haveRead").checked;
+        return new Book( authorValue, titleValue, pagesValue, isReadValue);
+    }
+
+    //Changes read property and CSS classes
+    toggleBtnRead(e) {
+        this.myLibrary[e.target.parentElement.getAttribute('data-index')].toggleRead();
+        e.target.textContent = this.myLibrary[e.target.parentElement.getAttribute('data-index')].isRead ? 'Read' : "Not read";
+        e.target.classList.toggle("have-read");
+    }
+
+    addBookToLibrary = (e) => {
+        //stops webpage from refreshing when submitting
+        e.preventDefault();
+        const newBook = this.getBookFromInput();
     
-}
+        this.myLibrary.push(newBook);
+        //create DOM book card
+        this.addAndDisplayBook(newBook);
+        //empties form upon submission
+        this.resetAndCloseForm();
+        
+    }
 
-//create DOM book card
-function addAndDisplayBook(newBook) {
+    deleteBook(e) {
+        //remove from array library
+        this.myLibrary.splice(e.currentTarget.parentElement.getAttribute('data-index'), 1);
+        //remove from DOM
+        e.currentTarget.parentElement.remove();
+        //adjust all data indexes to accurate resemble its index in library array
+        let cardItems = document.querySelectorAll('.card-item');
+        cardItems.forEach((cardItem) => {
+            if(cardItem.dataset.index == 0) return;
+            
+            else {
+                cardItem.dataset.index = cardItem.dataset.index - 1
+            }
+        });
+    }
+
+    //create DOM book card
+    addAndDisplayBook(newBook) {
         
     const libContainer = document.querySelector('.library-container');
     const cardItem = document.createElement('div');
@@ -56,10 +108,10 @@ function addAndDisplayBook(newBook) {
     if(newBook.isRead)  submitBtn.classList.add('have-read');
     
 
-    submitBtn.addEventListener('click', toggleBtnRead )
-    deleteIcon.addEventListener('click', deleteBook )
+    submitBtn.addEventListener('click', (e) => { this.toggleBtnRead(e)}) 
+    deleteIcon.addEventListener('click', (e) => { this.deleteBook(e)} )
 
-    cardItem.setAttribute('data-index', `${myLibrary.length - 1}`);
+    cardItem.setAttribute('data-index', `${this.myLibrary.length - 1}`);
     cardItem.appendChild(titlePara);
     cardItem.appendChild(deleteIcon);
     cardItem.appendChild(authorPara);
@@ -68,58 +120,20 @@ function addAndDisplayBook(newBook) {
 
     libContainer.appendChild(cardItem);
 }
-
-
-function getBookFromInput() {
-    let titleValue = document.querySelector("#title").value;
-    let authorValue = document.querySelector("#author").value;
-    let pagesValue = document.querySelector("#pages").value;
-    let isReadValue = document.querySelector("#haveRead").checked;
-    return new Book( authorValue, titleValue, pagesValue, isReadValue);
-}
-
-//Changes read property and CSS classes
-function toggleBtnRead(e) {
-    myLibrary[e.target.parentElement.getAttribute('data-index')].toggleRead();
-    e.target.textContent = myLibrary[e.target.parentElement.getAttribute('data-index')].isRead ? 'Read' : "Not read";
-    e.target.classList.toggle("have-read");
-}
-
-function deleteBook(e) {
-    //remove from array library
-    myLibrary.splice(e.currentTarget.parentElement.getAttribute('data-index'), 1);
-    //remove from DOM
-    e.currentTarget.parentElement.remove();
-    //adjust all data indexes to accurate resemble its index in library array
-    cardItems = document.querySelectorAll('.card-item');
-    cardItems.forEach((cardItem) => {
-        if(cardItem.dataset.index == 0) return;
-        
-        else {
-            cardItem.dataset.index = cardItem.dataset.index - 1
-        }
-    });
-}
-
-function resetAndCloseForm() {
-    form.reset();
-    const popup = document.querySelector('.popup-container');
-    popup.style.display ='none';
+    resetAndCloseForm() {
+        this.form.reset();
+        const popup = document.querySelector('.popup-container');
+        popup.style.display ='none';
+    }
 }
 
 
 
-const form = document.querySelector('.form');
-const newBookBtn = document.querySelector('.new-book-btn');
+const library = new Library();
+
+library.form.onsubmit = library.addBookToLibrary;
 
 
-form.onsubmit = addBookToLibrary;
-
-newBookBtn.addEventListener('click', () => {
-    const popup = document.querySelector('.popup-container');
-    popup.style.display ='block';
-    
-});
 
 //Clicking anywhere but the form closes it
 window.onclick = function (e) {
